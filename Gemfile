cca524931f57dd67fa53215f1492957c68961deb
Danyil Shkoropad <anko20094@gmail.com>
2022-04-07 16:27:06 +0300

    offline adding links + status messages

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "bootsnap", require: false
 
 gem "validate_url"
 gem 'public_suffix'
+gem 'jquery-rails'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,27 +1,38 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree
 
-import 'jquery'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 
-var $ = require( "jquery" );
+let $ = require( "jquery" );
 Rails.start()
 Turbolinks.start()
 
 $(document).on('turbolinks:load', function() {
 
+  window.addEventListener('offline', function(event){
+    alert("You lost connection.");
+
+  });
+  window.addEventListener('online', function(event){
+    alert("You are now back online.");
+    sendValueFromLocalStorageToDB();
+  });
+
   $(".btn").click(function () {
     let link = $("#link_name").val();
 
     if (validateUrl(link)) {
-      link = takeHostOfLink(link);
+      link = refactoringLink(link);
       sendValueToDB(link);
       createNewTags(link);
     } else {
       alert("Enter the valid link, please!");
     }
-
   });
 
   function createNewTags(link) {
@@ -44,11 +55,21 @@ $(document).on('turbolinks:load', function() {
     $.ajax({
       url: '/links',
       data: {'authenticity_token' : authToken, 'link' : {'name': link}},
-      type: "POST"
+      type: "POST",
+      error: function() {
+        sendValueToLocalStorage(link);
+        $("#warning").show();
+        setTimeout(function() { $("#warning").hide(); }, 3500);
+      },
+      success: function() {
+        $("#link_name").val('');
+        $("#success").show();
+        setTimeout(function() { $("#success").hide(); }, 3500);
+      }
     })
   }
 
-  function takeHostOfLink(link) {
+  function refactoringLink(link) {
     if (link.search('www.') >= 0) {
       return link.split('www.')[1].split('/')[0]
     } else if (link.search('http') >= 0){
@@ -56,5 +77,29 @@ $(document).on('turbolinks:load', function() {
     } else {
       return link.split('/')[0]
     }
+  }
+
+  function sendValueToLocalStorage(link) {
+    let dt = new Date();
+    let key = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    let authToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+    const data = {'authenticity_token' : authToken, 'link' : {'name': link}}
+    window.localStorage.setItem(String(key), JSON.stringify(data));
+  }
+
+  function sendValueFromLocalStorageToDB() {
+    for (let i = 0; i < localStorage.length; i++) {
+      let storedValue = localStorage.key(i);
+      $.ajax({
+        url: '/links',
+        data: JSON.parse(localStorage.getItem(storedValue)),
+        type: "POST",
+        success: function (){
+          localStorage.removeItem(storedValue)
+        }
+      });
+    }
+    $("#success").show();
+    setTimeout(function() { $("#success").hide(); }, 5500);
   }
 });
